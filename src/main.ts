519f8c6ee9975192e898077a08e3b83cc4bb09a2
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2025-02-26 09:22:38 +0100

    init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('api/v1');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  const config = new DocumentBuilder()
+    .setTitle('NestJs Full API')
+    .setDescription('A full NestJs Authentication')
+    .setVersion('1.0')
+    .addTag('Routes')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/v1', app, document);
+  await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
